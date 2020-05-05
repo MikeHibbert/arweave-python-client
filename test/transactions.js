9309541abb3de4736a710f2f7210b44c6a1bd217
mhibbert <hibbert.michael@gmail.com>
2020-05-05 23:00:00 +0100

    added deep_hash and merkle support for arweave v2.0

---
+++ test/transactions.js
@@ -0,0 +1,33 @@
+const Arweave = require('arweave/node');
+const fs = require('fs');
+
+const run_test = async function() {
+    const wallet_path = '/home/mike/Documents/python/arkive/arkive/wallet/arweave-keyfile-h-Bgr13OWUOkRGWrnMT0LuUKfJhRss5pfTdxHmNcXyw.json';
+    const arweave = Arweave.init(arweave_config = {
+        host: 'arweave.net',// Hostname or IP address for a Arweave node
+        port: 443,           // Port, defaults to 1984
+        protocol: 'https',  // Network protocol http or https, defaults to http
+        timeout: 200000,     // Network request timeouts in milliseconds
+        logging: false,     // Enable network request logging
+    });
+
+    const text = fs.readFileSync(wallet_path, 'utf8')
+    const jwk = JSON.parse(text);
+
+    let transaction = await arweave.createTransaction({
+        data: 'test'
+    }, jwk);
+
+    transaction.addTag('key1', 'value1');
+    transaction.addTag('key2', 'value2');
+
+    arweave.transactions.sign(transaction, jwk)
+
+    console.log(transaction.data);
+    console.log(transaction.data_root);
+    console.log(transaction.data_size);
+}
+
+run_test();
+
+
---
+++ test/transactions.py
@@ -0,0 +1,54 @@
+import os
+import logging
+from arweave.arweave_lib import Wallet, Transaction
+
+logger = logging.getLogger(__name__)
+
+
+def run_test(jwk_file):
+    wallet = Wallet(jwk_file)
+
+    DATA = b'dGVzdA'
+    DATA_ROOT = b'uwdqwpnoHTe237EIEIOizSYku8wkiVZJY-E6cv33wt4'
+
+    balance = wallet.get_balance()
+
+    logger.debug(balance)
+
+    tx = Transaction(wallet=wallet, data="test")
+
+    tx.add_tag('key1', 'value1');
+    tx.add_tag('key2', 'value2');
+
+    tx.sign()
+
+    logger.info(tx.data)
+    logger.info(tx.data_root)
+
+    if tx.data != DATA:
+        raise Exception("Data does not match expected result!")
+
+    if tx.data_root != DATA_ROOT:
+        raise Exception("Data root does not match expected result!")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
+    from os.path import isfile, join
+    from os import listdir
+
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+    wallet_path = '/home/mike/Documents/python/arkive/arkive/wallet'  # os.path.join(BASE_DIR, 'arkive', 'wallet')
+
+    files = [f for f in listdir(wallet_path) if isfile(join(wallet_path, f))]
+
+    if len(files) > 0:
+        filename = files[0]
+    else:
+        raise FileNotFoundError("Unable to load a wallet JSON file from wallet/ ")
+
+    run_test(join(wallet_path, filename))
